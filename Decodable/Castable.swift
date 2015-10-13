9eec14bf04dfcb645e807ff2b2ebed96cdce1a98
Ben Dolman <ben.dolman@gmail.com>
2015-10-12 23:55:21 -0600

    Auto convert strings to numbers where possible

--- Decodable/Castable.swift
+++ Decodable/Castable.swift
@@ -21,8 +21,37 @@ extension Castable {
     }
 }
 
+private let numberFormatter: NSNumberFormatter = {
+    let f = NSNumberFormatter()
+    f.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+    return f
+}()
+
+private func decodeNumeric<T>(j: AnyObject, primitiveClosure: (number: NSNumber) -> (T)) throws -> T {
+    if let result = j as? T {
+        return result
+    }
+    if let string = j as? String, result = numberFormatter.numberFromString(string) {
+        return primitiveClosure(number: result)
+    }
+    let info = DecodingError.Info(object: j)
+    throw DecodingError.TypeMismatch(type: j.dynamicType, expectedType: T.self, info: info)
+}
+
 extension String: Castable {}
-extension Int: Castable {}
-extension Double: Castable {}
-extension Bool: Castable {}
+extension Int: Castable {
+    public static func decode(j: AnyObject) throws -> Int {
+        return try decodeNumeric(j) { $0.integerValue }
+    }
+}
+extension Double: Castable {
+    public static func decode(j: AnyObject) throws -> Double {
+        return try decodeNumeric(j) { $0.doubleValue }
+    }
+}
+extension Bool: Castable {
+    public static func decode(j: AnyObject) throws -> Bool {
+        return try decodeNumeric(j) { $0.boolValue }
+    }
+}
 extension Dictionary: Castable {}
